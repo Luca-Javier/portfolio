28566dc3d9aa01275febe85b212ea516504edddb
Luca-Javier <luca.jav45@gmail.com>
2023-05-31 22:47:15 -0300

    config to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	server: {
+	site: "https://luca-javier.github.io",
+
+	/* server: {
 		host: true,
-	},
+	}, */
 })
