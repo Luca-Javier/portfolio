59da70968822cea0ee187cd93a30c3f43bbc9adf
Luca-Javier <luca.jav45@gmail.com>
2023-05-31 23:22:47 -0300

    df

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
 	site: "https://luca-javier.github.io",
-	base: "/portfolio",
 
 	/* server: {
 		host: true,
