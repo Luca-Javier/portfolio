ad114389b78cefdb252f6e747be9be52c2ab63b3
Luca-Javier <luca.jav45@gmail.com>
2023-05-31 23:04:26 -0300

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
 	site: "https://luca-javier.github.io",
-	base: "/portfolio",
+	base: "https://luca-javier.github.io/portfolio",
 
 	/* server: {
 		host: true,
